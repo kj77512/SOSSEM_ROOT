4328892fb63acb896d3bf470c85f96471a2107a0
iamsossem <iamsossem@gmail.com>
2023-07-05 14:40:59 +0900

    image update

--- 웹디자인기능사/실기연습/수험자비번/script/script.js
+++ 웹디자인기능사/실기연습/수험자비번/script/script.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    /*** header menu */
+    $('li.main-menu').mouseenter(function(){
+        $('ul.sub-menu').stop().slideDown();
+    });
+    $('li.main-menu').mouseleave(function(){
+        $('ul.sub-menu').stop().slideUp();
+    });
 })

e472f2b6a184a0fbe137028974eb5fea8725fec7
iamsossem <iamsossem@gmail.com>
2023-07-14 15:59:51 +0900

    commit

--- 웹디자인기능사/실기연습/C-1/script/script.js
+++ 웹디자인기능사/실기연습/C-1/script/script.js
@@ -3,5 +3,22 @@ $(function(){
     $('.tabMenu > li').click(function(){
         $('.tabMenu > li').removeClass('tabOn');
         $(this).addClass('tabOn');
+        /*** tabItem change */
+        let idx = $(this).index();
+        // console.log( idx );
+        $('.tabItem > ul').hide().removeClass('tabOn');
+        $('.tabItem > ul').eq(idx).show().addClass('tabOn');
+    });
+    /*** slide */
+    setInterval(function(){
+        $('.slide-item:first').fadeOut(2000).next().fadeIn(2000);
+        $('.slide-item:first').appendTo('.slide-wrap');
+    },3000);
+    /*** menu */
+    $('.main-menu').mouseenter(function(){
+        $(this).find('.sub-menu').stop().slideDown();
+    });
+    $('.main-menu').mouseleave(function(){
+        $(this).find('.sub-menu').stop().slideUp();
     });
 })

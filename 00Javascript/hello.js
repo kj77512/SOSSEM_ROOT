7f31fd1b663479e0be00e5d01411f416b4bcdf57
iamsossem <iamsossem@gmail.com>
2023-07-19 17:49:11 +0900

    update

---
+++ 00Javascript/hello.js
@@ -0,0 +1,31 @@
+/***
+ * 경고창을 보여줍니다.
+ * alert를 사용하여 
+ */
+/***
+alert('hello');
+alert('world');
+ */
+
+/***
+ * 변수 사용하기
+ * let - 변경이 가능한 변수, 
+ * const - 변경이 불가능한 상수
+ * 10 + 20
+ */
+/***
+ * 데이터 타입 : 숫자, 문자열, 불린형(true,false)
+ */
+// let result = 10+20;
+// result = "aaa";
+// result = 5>6;
+// // document.write(result);
+// console.log( result );
+// console.log( typeof(result) );
+// console.log( "---------------")
+// //null과 undefined
+// console.log( data );
+
+
+value = 10;
+var value=5;

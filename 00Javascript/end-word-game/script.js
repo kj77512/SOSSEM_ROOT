c4a4a8f728f4a7115696a3a9e8301fc20c8d9d2d
iamsossem <iamsossem@gmail.com>
2023-08-17 10:13:46 +0900

    commit

--- 00Javascript/end-word-game/script.js
+++ 00Javascript/end-word-game/script.js
@@ -1,12 +1,33 @@
 //변수 정의
 // const MAX_USER = Number( prompt("몇명이 참가하나요?") );
 const MAX_USER = 3;
+const arrHistory = [];
 
 //객체 정의
 const elemInput = document.querySelector('input');
 const elemBtn = document.querySelector('button');
 const elemWord = document.querySelector('.word');
+const elemHistory = document.querySelector('.history');
+const elemOrder = document.querySelector('.order');
 //함수 정의
+//history를 추가해 주는 함수
+const addStrHistory = (str) => {
+    arrHistory.push(str);
+    let view = '';
+    arrHistory.forEach( (value) => {
+        view += value + '->';
+    } );
+    elemHistory.textContent = view;
+}
+//order를 변경해 주는 함수 
+const changeOrder = () => {
+    let idx = Number(elemOrder.textContent);
+    if( idx+1 > MAX_USER ){
+        elemOrder.textContent = 1;
+    } else {
+        elemOrder.textContent = idx+1;
+    }
+}
 const onClickBtn = () => {
     //1. input의 value 값을 먼저 읽어오기
     let strInput = elemInput.value;
@@ -17,6 +38,10 @@ const onClickBtn = () => {
             //끝말잇기가 가능
             //글씨가 있는 경우 : 제시어 옆에 표시
             elemWord.textContent = strInput;  
+            //history도 남겨져야 한다.
+            addStrHistory(strInput);
+            //order 숫자 변경
+            changeOrder();
         } else {
             alert("올바르지 않은 단어입니다");
         }              

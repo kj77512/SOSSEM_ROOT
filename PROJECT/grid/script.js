f469102c67498103d017d0f73073644f9aaf5adf
iamsossem <iamsossem@gmail.com>
2023-08-17 15:23:30 +0900

    commit

---
+++ PROJECT/grid/script.js
@@ -0,0 +1,21 @@
+//객체 선언
+const elemBar = document.querySelector('nav');
+const elemMenuList = document.querySelectorAll('nav>ul>li');
+const elemIcon = document.querySelector('.menu-icon');
+
+//메뉴 리스트에서 클릭을 하면 실행되는 함수
+const onClickMenu = () => {
+    // console.log( "menu list click" );
+    //menubar 에 class=menu-off 추가 되도록    
+    elemBar.classList.toggle('menu-off');  
+    //menu-icon 에 call=icon-on 추가 되도록  
+    elemIcon.classList.toggle('icon-on');
+}
+const init = () => {
+    //처음 시작 함수
+    elemMenuList.forEach( (value) =>{
+        value.addEventListener('click',onClickMenu );
+    } );
+    elemIcon.addEventListener('click',onClickMenu);
+}
+init();
